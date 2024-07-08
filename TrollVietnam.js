142256831345c1e91658d82a3d8024196f05276a
MaiThanhNam141 <113810037+MaiThanhNam141@users.noreply.github.com>
2024-07-08 11:04:21 +0700

    Update TrollVietnam.js

--- TrollVietnam.js
+++ TrollVietnam.js
@@ -1,9 +1,9 @@
 async function HackingFunction() {
     let timeout;
     for (let i = 0; i <= 100; i++) {
-        timeout = Math.floor(Math.random()*90);
-        await new Promise((resolve) => setTimeout(resolve, timeout*timeout));
+        timeout = Math.floor(Math.random()*100);
         console.log(`Hacking... ${i}%`);
+        await new Promise((resolve) => setTimeout(resolve, timeout*timeout));
     }
     console.log("Hacking Completed!");
 }
